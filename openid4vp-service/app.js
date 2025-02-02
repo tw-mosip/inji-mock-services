ec3c5df36bbb3b443104e215a21c7793a277e077
Alka Prasad <Alka1703@users.noreply.github.com>
2025-02-02 23:24:03 +0530

    [INJIMOB-2761]: expose api to accept auth request as value and reference in openid4vp services (#4)
    * [INJIMOB-2761]: expose api to accept auth request as value and reference in openid4vp services
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: remove base6r encoding of jwt
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: stringifying the client metadata whicle sending in auth request
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: update the readme with latest changes
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: update did document url in the mock service
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: refactor code
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: update the readme
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2761]: update the client metadata
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    ---------
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- openid4vp-service/app.js
+++ openid4vp-service/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const QRCode = require('qrcode');
-const crypto = require('crypto');
 const presentationDefinition = require('./presentationDefinitionMock.json');
+const clientMetadata = require('./clientMetadataMock.json');
 const bodyParser = require('body-parser');
+const {createJWT, jwtPayload} = require("./jwt");
 const app = express();
+const {state, nonce,requestUri, responseUri, didDocumentUrl, publicKeyId} = require("./constants");
 const PORT = 3000;
 
-var nonce, state;
+
 
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 app.set('view engine', 'ejs');
@@ -16,25 +18,43 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/verifier/generate-auth-request-qr', async (req, res) => {
+app.get('/verifier/generate-auth-request-by-value-qr', async (req, res) => {
   try {
     const presentation_definition = JSON.stringify(presentationDefinition);
-    const client_metadata = JSON.stringify({ 'client_name': 'Requester name' });
-    nonce = crypto.randomBytes(16).toString('base64');
-    state = crypto.randomBytes(16).toString('base64');
+    const client_metadata = JSON.stringify(clientMetadata);
 
-    //To send presentation_definition_uri instead of presentation_definition replace the presentation_definition with the below presentation_definition_uri in the Authorization request
-    //presentation_definition_uri=https://6671-2405-201-c058-b814-3935-591-c028-f58e.ngrok-free.app/verifier/presentation_definition_uri
-    const authorizationRequest = `client_id=https://injiverify.dev1.mosip.net&presentation_definition=${presentation_definition}&response_type=vp_token&response_mode=direct_post&nonce=${nonce}&state=${state}&response_uri=https://f0c2-2405-201-c058-b814-f1e4-f5de-f8a0-abbd.ngrok-free.app/verifier/vp-response&client_metadata=${client_metadata}`;
-    const qrCodeData = await QRCode.toDataURL(
-      'openid4vp://authorize?' + btoa(authorizationRequest)
-    );
+    const authorizationRequest = `client_id=https://injiverify.dev1.mosip.net&presentation_definition=${presentation_definition}&response_type=vp_token&response_mode=direct_post&nonce=${nonce}&state=${state}&response_uri=${responseUri}&client_metadata=${client_metadata}&client_id_scheme=pre-registered`;
+    const qrCodeData = await QRCode.toDataURL('openid4vp://authorize?' + btoa(authorizationRequest));
 
     res.render('index', { title: 'Home', qrCodeData });
   } catch (error) {
     console.error('Error generating QR code:', error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+
+app.get('/verifier/generate-auth-request-by-reference-qr', async (req, res) => {
+    try {
+        const authorizationRequest = `client_id=${didDocumentUrl}&client_id_scheme=did&request_uri=${requestUri}&request_uri_method=get`;
+        const qrCodeData = await QRCode.toDataURL('openid4vp://authorize?' + btoa(authorizationRequest));
+        res.render('index', {title: 'Home', qrCodeData});
+    } catch (error) {
+        console.error('Error generating QR code:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+//openid4vp://authorize?client_id=https://injiverify.dev1.mosip.net&presentation_definition=
+
+app.get('/verifier/get-auth-request-obj', async (req, res) => {
+    try {
+        const jwt = await createJWT()
+        res.send(jwt)
+        //res.send(btoa(JSON.stringify(jwtPayload)))
+    } catch (error) {
+        console.error('Error generating JWT :', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/verifier/presentation_definition_uri', async (req, res) => {
---
+++ openid4vp-service/jwt.js
@@ -0,0 +1,60 @@
+const ed = require('@noble/ed25519');
+const forge = require('node-forge');
+const presentationDefinition = require('./presentationDefinitionMock.json');
+
+const clientMetadata = require('./clientMetadataMock.json');
+const {baseUrl, state, nonce, ed25519PrivateKey, responseUri, didDocumentUrl, publicKeyId} = require("./constants");
+
+const client_metadata = clientMetadata
+
+const jwtPayload = {
+    "presentation_definition_uri": `${baseUrl}/verifier/presentation_definition_uri`,
+    "client_metadata": JSON.stringify(client_metadata),
+    "state": state,
+    "nonce": nonce,
+    "client_id": didDocumentUrl,
+    "client_id_scheme": "did",
+    "response_mode": "direct_post",
+    "response_type": "vp_token",
+    "response_uri": responseUri
+}
+
+const jwtHeader =   {
+    "typ": "oauth-authz-req+jwt",
+    "alg": "EdDSA",
+    "kid": publicKeyId
+}
+
+function replaceCharactersInB64(encodedB64) {
+    return encodedB64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+function encodeB64(str) {
+    const encodedB64 = forge.util.encode64(str);
+    return replaceCharactersInB64(encodedB64);
+}
+
+async function createSignatureED(privateKey, prehash) {
+    const messageBytes = new TextEncoder().encode(prehash);
+    const privateKeyUint8 = Uint8Array.from(privateKey);
+    const sign = await ed.sign(messageBytes, privateKeyUint8);
+    return replaceCharactersInB64(Buffer.from(sign).toString('base64'));
+}
+
+
+async function createJWT() {
+
+    const header64 = encodeB64(JSON.stringify(jwtHeader));
+    const payLoad64 = encodeB64(JSON.stringify(jwtPayload));
+    const preHash = header64 + '.' + payLoad64;
+    const privateKey = Uint8Array.from(Buffer.from(ed25519PrivateKey, 'base64'));
+    const signature64 = await createSignatureED(privateKey, preHash)
+    return header64 + '.' + payLoad64 + '.' + signature64;
+
+}
+
+module.exports = {
+    createJWT,
+    client_metadata,
+    jwtPayload
+};
