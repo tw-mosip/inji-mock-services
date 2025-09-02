02e80de7e3a62dc3014a4dbcd7cf6374474e406e
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-09-02 14:48:40 +0530

    [INJIMOB-3455] refactor: update final input data
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- openid4vp-service/inputData.js
+++ openid4vp-service/inputData.js
@@ -1,4 +1,5 @@
-const {nonce, state, responseUri, baseUrl, didDocumentUrl, requestUri, clientId, presentationDefinitionUri,
+const {
+    nonce, state, responseUri, baseUrl, didDocumentUrl, requestUri, clientId, presentationDefinitionUri,
     CLIENT_ID_SCHEMES, SUPPORT_TYPES, REQUEST_MODES, DRAFT_VERSIONS
 } = require("./constants");
 const clientMetadata = require('./clientMetadataMock.json');
@@ -32,7 +33,7 @@ const redirectAuthorizationRequestDraft23 = {
     "client_id": `redirect_uri:${responseUri}`,
     "presentation_definition_uri": presentationDefinitionUri,
     "response_type": "vp_token",
-    "response_mode": "direct_post.jwt",
+    "response_mode": "direct_post",
     "nonce": nonce,
     "state": state,
     "response_uri": responseUri,
@@ -75,26 +76,52 @@ const didAuthorizationRequestDraft21 = {
 }
 
 const authorizationRequestParamsDraft23 = {
-    "client_id":didDocumentUrl,
+    "client_id": didDocumentUrl,
     "request_uri": requestUri,
     "request_uri_method": "post"
 }
 
 const authorizationRequestParamsDraft21 = {
-    "client_id":didDocumentUrl,
+    "client_id": didDocumentUrl,
+    "client_id_scheme": "did",
+    "request_uri": requestUri,
+    "request_uri_method": "post"
+}
+
+const preRegisteredAuthorizationRequestParamsDraft23 = {
+    "client_id": preRegisteredAuthorizationRequestDraft23.client_id,
+    "request_uri": requestUri,
+    "request_uri_method": "post"
+}
+
+const preRegisteredAuthorizationRequestParamsDraft21 = {
+    "client_id": preRegisteredAuthorizationRequestDraft21.client_id,
+    "client_id_scheme": "did",
+    "request_uri": requestUri,
+    "request_uri_method": "post"
+}
+
+const redirectUriAuthorizationRequestParamsDraft23 = {
+    "client_id": redirectAuthorizationRequestDraft23.client_id,
+    "request_uri": requestUri,
+    "request_uri_method": "post"
+}
+
+const redirectUriAuthorizationRequestParamsDraft21 = {
+    "client_id": redirectAuthorizationRequestDrat21.client_id,
     "client_id_scheme": "did",
     "request_uri": requestUri,
     "request_uri_method": "post"
 }
 
 const didAuthorizationRequestParamsDraft23 = {
-    "client_id":didDocumentUrl,
+    "client_id": didDocumentUrl,
     "request_uri": requestUri,
     "request_uri_method": "post"
 }
 
 const didAuthorizationRequestParamsDraft21 = {
-    "client_id":didDocumentUrl,
+    "client_id": didDocumentUrl,
     "client_id_scheme": "did",
     "request_uri": requestUri,
     "request_uri_method": "post"
@@ -106,8 +133,8 @@ const finalAuthRequestMap = {
         [SUPPORT_TYPES.SUPPORTS_BY_REFERENCE]: true,
         [SUPPORT_TYPES.SUPPORTS_BY_VALUE]: true,
         [REQUEST_MODES.BY_REFERENCE]: {
-            [DRAFT_VERSIONS.DRAFT_23]: authorizationRequestParamsDraft23,
-            [DRAFT_VERSIONS.DRAFT_21]: authorizationRequestParamsDraft23,
+            [DRAFT_VERSIONS.DRAFT_23]: preRegisteredAuthorizationRequestParamsDraft23,
+            [DRAFT_VERSIONS.DRAFT_21]: preRegisteredAuthorizationRequestParamsDraft21,
         },
         [REQUEST_MODES.BY_VALUE]: {
             [DRAFT_VERSIONS.DRAFT_23]: preRegisteredAuthorizationRequestDraft23,
@@ -118,8 +145,8 @@ const finalAuthRequestMap = {
         [SUPPORT_TYPES.SUPPORTS_BY_REFERENCE]: false,
         [SUPPORT_TYPES.SUPPORTS_BY_VALUE]: true,
         [REQUEST_MODES.BY_REFERENCE]: {
-            [DRAFT_VERSIONS.DRAFT_23]: authorizationRequestParamsDraft23,
-            [DRAFT_VERSIONS.DRAFT_21]: authorizationRequestParamsDraft23,
+            [DRAFT_VERSIONS.DRAFT_23]: redirectUriAuthorizationRequestParamsDraft23,
+            [DRAFT_VERSIONS.DRAFT_21]: redirectUriAuthorizationRequestParamsDraft21,
         },
         [REQUEST_MODES.BY_VALUE]: {
             [DRAFT_VERSIONS.DRAFT_23]: redirectAuthorizationRequestDraft23,
@@ -131,7 +158,7 @@ const finalAuthRequestMap = {
         [SUPPORT_TYPES.SUPPORTS_BY_VALUE]: false,
         [REQUEST_MODES.BY_REFERENCE]: {
             [DRAFT_VERSIONS.DRAFT_23]: didAuthorizationRequestParamsDraft23,
-            [DRAFT_VERSIONS.DRAFT_21]: didAuthorizationRequestParamsDraft23,
+            [DRAFT_VERSIONS.DRAFT_21]: didAuthorizationRequestParamsDraft21,
         },
         [REQUEST_MODES.BY_VALUE]: {
             [DRAFT_VERSIONS.DRAFT_23]: didAuthorizationRequestDraft23,
