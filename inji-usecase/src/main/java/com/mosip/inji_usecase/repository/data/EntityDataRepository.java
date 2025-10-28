c219354fe344e0823c15549fe6f066472f533eb1
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-10-28 21:10:06 +0530

    refactor: remove farmer related classes
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- inji-usecase/src/main/java/com/mosip/inji_usecase/repository/data/EntityDataRepository.java
+++ inji-usecase/src/main/java/com/mosip/inji_usecase/repository/data/EntityDataRepository.java
@@ -1,12 +1,11 @@
 package com.mosip.inji_usecase.repository.data;
 
 import com.mosip.inji_usecase.entity.data.EntityData;
-import com.mosip.inji_usecase.entity.farmer.Farmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntityDataRepository extends JpaRepository<EntityData, Long>, JpaSpecificationExecutor<Farmer> {
+public interface EntityDataRepository extends JpaRepository<EntityData, Long>, JpaSpecificationExecutor<EntityData> {
 
 }
