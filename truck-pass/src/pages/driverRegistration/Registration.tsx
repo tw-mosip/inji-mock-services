3b114ae6e4126bc55021b1e1c474ade09461e848
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-01-31 08:44:47 +0530

    refactor: update api for driver details update (#129)
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    Co-authored-by: Swati Goel <meet2swati@gmail.com>

--- truck-pass/src/pages/driverRegistration/Registration.tsx
+++ truck-pass/src/pages/driverRegistration/Registration.tsx
@@ -56,7 +56,7 @@ export const Registration: React.FC<RegistrationProps> = ({ }) => {
         }
     }, []);
 
-    const { post_driver_registration } = { ...relyingPartyService }
+    const { post_driver_details } = { ...relyingPartyService }
 
     const moveToVerifyUinPage = () => {
         navigate('/driverRegistrationProcessPage/verifyUINPage');
@@ -124,7 +124,7 @@ export const Registration: React.FC<RegistrationProps> = ({ }) => {
         try {
             setRegistrationScreen(false);
             setRegistrationSubmitBtn(true);
-            const response = await post_driver_registration('/drivers/register', driverRegistrationFormData);
+            const response = await post_driver_details(driverRegistrationFormData);
             if (response) {
                 const driverAdditionalFiles = {
                     driverPicture: driverInfo?.picture,
